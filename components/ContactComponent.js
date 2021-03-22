6a3036d13689a7b50913716dce6bb69f99b05200
Michele Ortiz <micheleyortiz@gmail.com>
2021-03-22 15:51:09 -0400

    Sending Email

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
-import { ScrollView, Text} from 'react-native';
+import { ScrollView, Text, Button, Icon} from 'react-native';
 import { Card } from 'react-native-elements';
+import * as MailComposer from 'expo-mail-composer';
 
 class Contact extends Component {
     static navigationOptions = {
         title: 'Contact Us'
     }
+    sendMail() {
+        MailComposer.composeAsync({
+            recipients: ['campsites@nucamp.co'],
+            subject: 'Inquiry',
+            body: 'To whom it may concern:'
+        })
+    }
+
     render() {
         return(
             <ScrollView>
@@ -18,6 +27,17 @@ class Contact extends Component {
                  <Text style={{marginBottom: 10}}>U.S.A.</Text> 
                  <Text>Phone: 1-678-327-4505</Text>
                  <Text>Email: Flowerpower@gmail.com</Text>
+                 <Button
+                            title="Send Email"
+                            buttonStyle={{backgroundColor: '#2e8b57', margin: 40}}
+                            icon={<Icon
+                                name='envelope-o'
+                                type='font-awesome'
+                                color='#fff'
+                                iconStyle={{marginRight: 10}}
+                            />}
+                            onPress={() => this.sendMail()}
+                        />
                 </Card>
             </ScrollView>
         )
